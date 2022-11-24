90da30d59381e6d0fd6a1a2bd4961405bcb7a7be
frodrigue60 <frodrigue60@gmail.com>
2022-11-23 22:17:03 -0600

    error 404 if post doesnt exist

--- resources/views/fromTags.blade.php
+++ resources/views/fromTags.blade.php
@@ -38,7 +38,7 @@
                                 @endif
                             @endauth
 
-                            <button class="btn btn-primary">{{ $post->averageRating * 10 }}
+                            <button class="btn btn-primary">{{ $post->averageRating / 10 }}
 
                                 <svg style="color: white" xmlns="http://www.w3.org/2000/svg" width="16" height="16"
                                     fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
