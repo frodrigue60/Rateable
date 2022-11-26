aa31044f6ed82d477949ea78c56568fa05f6ea87
frodrigue60 <frodrigue60@gmail.com>
2022-11-26 16:14:43 -0600

    changes in css

--- resources/views/fromTags.blade.php
+++ resources/views/fromTags.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="contenedor">
-        <div class="contenedor-tarjetas">
+    <div class="container">
+        <div class="contenedor-favoritos">
             @foreach ($posts as $post)
                 <div class="tarjeta" style="background-image: url('{{ $post->imagesrc }}')">
                     <div class="textos">
@@ -11,53 +11,25 @@
                         </div>
                         <div class="tarjeta-footer">
                             <a href="{{ route('show', $post->id) }}" class="btn btn-primary"> Ver</a>
-
                             @auth
                                 @if ($post->liked())
                                     <form action="{{ route('unlike.post', $post->id) }}" method="post">
                                         @csrf
-                                        <button class="btn btn-danger">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15"
-                                                fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
-                                                <path fill-rule="evenodd"
-                                                    d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
-                                            </svg>
-                                        </button>
+                                        <button class="btn btn-danger"><i class="fa fa-heart"></i></button>
                                     </form>
                                 @else
                                     <form action="{{ route('like.post', $post->id) }}" method="post">
                                         @csrf
-                                        <button class="btn btn-success">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15"
-                                                fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
-                                                <path fill-rule="evenodd"
-                                                    d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
-                                            </svg>
-                                        </button>
+                                        <button class="btn btn-success"><i class="fa fa-heart"></i></button>
                                     </form>
                                 @endif
                             @endauth
-
-                            <button class="btn btn-primary">{{ $post->averageRating / 10 }}
-
-                                <svg style="color: white" xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                    fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
-                                    <path
-                                        d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"
-                                        fill="white"></path>
-                                </svg>
-
-                            </button>
+                            <button class="btn btn-primary">{{ $post->averageRating / 10 }} <i
+                                    class="fa fa-star"></i></button>
                         </div>
-
-
                     </div>
                 </div>
             @endforeach
-
-
         </div>
-
-        
     </div>
 @endsection
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -8,52 +8,47 @@
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
         @endif
-        <div class="row">
-            {{-- IF AUTH USER --}}
-            @auth
-                <div class="col-8">
+        {{-- IF AUTH USER --}}
+        @auth
+            <div class="row row-cols-1 row-cols-md-2 g-4">
+                <div class="col">
                     <div class="card">
                         <div class="card-header">
                             <div class="row justify-content-between">
                                 <div class="col-9">
-                                    <h5>{{ $post->title }}</h5>
-                                    
+                                    <h5 class="card-title">{{ $post->title }}</h5>
                                 </div>
                                 <div class="col-3">
-                                    @auth
-                                        @if ($post->liked())
-                                            <form action="{{ route('unlike.post', $post->id) }}" method="post">
-                                                @csrf
-                                                <button class="btn btn-sm btn-danger" id="like">Favorite <i class="fa fa-heart"></i></button>
-                                            </form>
-                                        @else
-                                            <form action="{{ route('like.post', $post->id) }}" method="post">
-                                                @csrf
-                                                <button class="btn btn-sm btn-success" id="like">Favorite <i class="fa fa-heart"></i></button>
-                                            </form>
-                                        @endif
-                                    @endauth
+                                    @if ($post->liked())
+                                        <form action="{{ route('unlike.post', $post->id) }}" method="post">
+                                            @csrf
+                                            <button class="btn btn-sm btn-danger" id="like">Favorite <i
+                                                    class="fa fa-heart"></i></button>
+                                        </form>
+                                    @else
+                                        <form action="{{ route('like.post', $post->id) }}" method="post">
+                                            @csrf
+                                            <button class="btn btn-sm btn-success" id="like">Favorite <i
+                                                    class="fa fa-heart"></i></button>
+                                        </form>
+                                    @endif
                                 </div>
                             </div>
-                            <div class="card-body">
-                                <div class="card-body ratio ratio-16x9">
-                                    <iframe id="id_iframe" src="{{ $post->ytlink }}" title="" frameborder="0"
-                                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture muted;"
-                                        allowfullscreen>
-                                    </iframe>
-                                </div>  
-                            </div>
-                            <div class="card-footer">
-                            </div>
+                        </div>
+                        <div class="card-body ratio ratio-16x9">
+                            <iframe width="560" height="315" src="{{ $post->ytlink }}" title="YouTube video player"
+                                frameborder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                allowfullscreen>
+                            </iframe>
+                        </div>
+                        <div class="card-footer">
                         </div>
                     </div>
-                </div>
-
-                {{-- RIGHT PANNEL --}}
-                <div class="col-4">
+                    {{-- RIGHT PANNEL --}}
                     <div class="card">
                         {{-- CARD HEADER --}}
-                        <div class="card-header text-dark bg-light">
+                        <div class="card-header">
                             @foreach ($post->tags as $tag)
                                 <span class="badge rounded-pill text-bg-dark">{{ $tag->name }}</span>
                             @endforeach
@@ -63,15 +58,16 @@
                         <div class="card-body">
                             <div class="row text-center">
                                 <h2>Average Score</h2>
-                                <br>
                                 <h3>{{ $post->averageRating / 1 }}</h3>
+                                <h4>Song title: <strong>a song title</strong></h4>
+                                <h4>Song artist: <strong>a artist</strong></h4>
                             </div>
                             <form name="add-blog-post-form" id="add-blog-post-form" method="post"
                                 action="{{ route('post.addrate', $post->id) }}" enctype="multipart/form-data">
                                 @csrf
                                 <label for="scoreInput" class="form-label">Score 0 - 100</label>
                                 <input type="number" class="form-control" id="scoreInput" placeholder="Score" id="score"
-                                    name="score" max="100" min="0">
+                                    name="score" max="100" min="0" value="{{ $post->userAverageRating / 1 }}">
                                 <br>
                                 <div class="row">
                                     <button type="submit" class="btn btn-primary">Rate</button>
@@ -87,69 +83,37 @@
                                 <a name="" id="" class="btn btn-success" href="#" role="button">Apple
                                     Music</a>
                             </div>
-
                         </div>
                         {{-- CARD FOOTER --}}
                         <div class="card-footer">
-
                         </div>
                     </div>
                 </div>
-            @endauth
-            {{-- IF GUEST --}}
-            @guest
-                <div class="col-10 mx-auto">
-                    <div class="card">
-                        <div class="card-header">
-                            <div class="row justify-content-between">
-                                <div class="col-10">
-                                    <h6>{{ $post->title }}</h6>
-                                </div>
-                                <div class="col-2">
-                                    @auth
-                                        @if ($post->liked())
-                                            <form action="{{ route('unlike.post', $post->id) }}" method="post">
-                                                @csrf
-                                                <button class="btn btn-danger">Favorite
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15"
-                                                        fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
-                                                        <path fill-rule="evenodd"
-                                                            d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
-                                                    </svg>
-                                                </button>
-                                            </form>
-                                        @else
-                                            <form action="{{ route('like.post', $post->id) }}" method="post">
-                                                @csrf
-                                                <button class="btn btn-success">Favorite
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15"
-                                                        fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
-                                                        <path fill-rule="evenodd"
-                                                            d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
-                                                    </svg>
-                                                </button>
-                                            </form>
-                                        @endif
-                                    @endauth
-                                </div>
-                            </div>
 
-                            <div class="card-body ratio ratio-16x9">
-                                <iframe id="id_iframe" src="{{ $post->ytlink }}" title="" frameborder="0"
-                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture muted;"
-                                    allowfullscreen>
-                                </iframe>
-                            </div>
-                            <div class="card-footer">
-                                <a name="" id="" class="btn btn-success" href="#"
-                                    role="button">Spotify</a>
-                                <a name="" id="" class="btn btn-success" href="#"
-                                    role="button">Apple Music</a>
-                            </div>
-                        </div>
+            </div>
+
+        @endauth
+
+        {{-- IF GUEST --}}
+        @guest
+            <div class="col-9 mx-auto">
+                <div class="card">
+                    <div class="card-header">
+                        <h5 class="card-title">{{ $post->title }}</h5>
+                    </div>
+                    <div class="card-body ratio ratio-16x9">
+                        <iframe id="id_iframe" src="{{ $post->ytlink }}" title="" frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture muted;"
+                            allowfullscreen>
+                        </iframe>
+                    </div>
+                    <div class="card-footer">
+                        <a name="" id="" class="btn btn-success" href="#" role="button">Spotify</a>
+                        <a name="" id="" class="btn btn-success" href="#" role="button">Apple Music</a>
                     </div>
                 </div>
-            @endguest
-        </div>
+            </div>
+        @endguest
+
     </div>
 @endsection
