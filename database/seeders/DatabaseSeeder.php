3822dad88f6e8b88a10f796a74a8c02f8664e3e6
frodrigue60 <frodrigue60@gmail.com>
2022-11-30 13:01:16 -0600

    seeder user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,5 +22,12 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]); 
+        $password = 'a12edc21cd';
+        $post = new User;
+        $post->name = 'Luis Rodz';
+        $post->email = 'frodrigue60@gmail.com';
+        $post->password = bcrypt($password);
+        $post->type = 'admin';
+        $post->save();
     }
 }
