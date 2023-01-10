7d6ec444811679c8928d0b0bc4ff9246289a5483
frodrigue60 <frodrigue60@gmail.com>
2023-01-10 15:03:07 -0600

    added production and local enviorements

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,22 +31,31 @@
 
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet">
-    {{-- <link rel="stylesheet" href="{{ asset('/resources/bootstrap-5.2.3-dist/css/bootstrap.min.css') }}"> --}}
     <link rel="stylesheet" href="{{ asset('/resources/owlcarousel/assets/owl.carousel.min.css') }}">
     <link rel="stylesheet" href="{{ asset('/resources/owlcarousel/assets/owl.theme.default.min.css') }}">
+    @production
+        <link rel="stylesheet" href="{{ asset('/resources/bootstrap-5.2.3-dist/css/bootstrap.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('/build/assets/modalSearch.css') }}">
+        <link rel="stylesheet" href="{{ asset('/build/assets/app.css') }}">
+    @endproduction
 
-    {{-- <link rel="stylesheet" href="{{ asset('/build/assets/modalSearch.00eba843.css') }}">
-    <link rel="stylesheet" href="{{ asset('/build/assets/app.243c9c4b.css') }}"> --}}
 
     <!-- JS -->
     <script src="{{ asset('/resources/js/pwa-script.js') }}"></script>
     <script src="{{ asset('/resources/js/jquery-3.6.3.min.js') }}"></script>
-    {{-- <script src="{{ asset('/resources/bootstrap-5.2.3-dist/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('/resources/js/popper.min.js') }}"></script> --}}
+    @production
+        <script src="{{ asset('/resources/bootstrap-5.2.3-dist/js/bootstrap.bundle.min.js') }}"></script>
+        <script src="{{ asset('/resources/js/popper.min.js') }}"></script>
+    @endproduction
+
     <script src="{{ asset('/resources/owlcarousel/owl.carousel.min.js') }}"></script>
+
+    @env('local')
     @vite(['resources/sass/app.scss', 'resources/js/app.js', 'resources/js/ajaxSearch.js', 'resources/css/app.css', 'resources/css/modalSearch.css'])
+    @endenv
 
 </head>
 
@@ -56,7 +65,7 @@
             <div class="spinner"></div>
         </div>
         @include('layouts.navbar')
-        <main class="py-4">
+        <main class="py-2">
             @include('layouts.alerts')
             @yield('content')
             {{-- Modal Search --}}
@@ -64,7 +73,9 @@
         </main>
         @include('layouts.footer')
     </div>
-    {{-- <script src="{{ asset('/build/assets/ajaxSearch.2bf24ff5.js') }}"></script> --}}
+    @production
+        <script src="{{ asset('/build/assets/ajaxSearch.js') }}"></script>
+    @endproduction
 </body>
 
 </html>
