9e199ed4661f4f62024324ea3bd3988edb264f11
frodrigue60 <frodrigue60@gmail.com>
2023-08-17 23:50:53 -0600

    restored db conf

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DATABASE_URL', 'mysql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
