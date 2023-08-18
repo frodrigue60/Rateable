18ec026305edb3fb901008bbd76dd143834b55a1
frodrigue60 <frodrigue60@gmail.com>
2023-08-18 01:40:47 -0600

    added extra conf

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,5 +28,12 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
+
+        if (!App::environment([
+            'local',
+            'testing',
+        ])) {
+            URL::forceScheme('https');
+        }
     }
 }
